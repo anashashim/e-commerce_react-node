17905220490eb51c0a221708c9d8cefb65d421f0
Anas Pola <anas@542.digital>
2021-12-28 14:26:43 +0530

    Backend initialization of code

---
+++ server/src/config/db.js
@@ -0,0 +1,18 @@
+import mongose from 'mongoose'
+
+// a mongoose stuf (mongoose.connect ....) return always a promise
+const connectDB = async () => {
+    try{
+        const conn = await mongose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+      console.error(`Error: ${error.message}`.red.underline.bold)
+            process.exit(1)
+    }
+}
+
+export default connectDB
